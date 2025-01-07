3bf26c45940f8412f20ace2568a45c5250ab304b
seyun0714 <65654463+seyun0714@users.noreply.github.com>
2025-01-07 15:40:45 +0900

    유저 구분 및 상대방에게 메시지 보내기 구현
    TODO : createAt 통일

--- server.js
+++ server.js
@@ -14,26 +14,35 @@ const clients = new Set();
 wss.on('connection', (ws) => {
     clients.add(ws);
     console.log('클라이언트 접속');
-
     ws.on('message', async (message) => {
         try{
             const data = JSON.parse(message);
-            const now = Date.now();
 
-            await dynamo.put({
-                TableName: process.env.TABLE_NAME || 'messages',
-                Item: {
-                    room: "default",
-                    createAt: now,
-                    user: data.user,
-                    text: data.text
-                }
-            }).promise();
+            if(data.type === 'init'){
+                ws.userId = data.userId;
+                console.log(`초기연결, userId : ${ws.userId}`);
+                return;
+            }
 
-            const broadcastMsg = { ...data, room: "default", createAt: now};
-            for(const client of clients){
-                if(client.readyState === WebSocket.OPEN){
-                    client.send(JSON.stringify(broadcastMsg));
+            if(data.type === 'chat'){
+                console.log(data.text);
+                const now = data.createAt;
+
+                /*await dynamo.put({
+                    TableName: process.env.TABLE_NAME || 'messages',
+                    Item: {
+                        room: "default",
+                        createAt: now,
+                        user: data.user,
+                        text: data.text
+                    }
+                }).promise();*/
+
+                const broadcastMsg = { ...data, room: "default", createAt: now};
+                for(const client of clients){
+                    if(client.readyState === WebSocket.OPEN && client !== ws){
+                        client.send(JSON.stringify(broadcastMsg));
+                    }
                 }
             }
         } catch (err){
